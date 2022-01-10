6a1ef2e55316c90df55384b531e735426bc79f33
Your Name <trangiahuytg1@gmail.com>
2022-01-10 14:23:42 +0700

    up lần đầu

---
+++ src/BankNonStrucure.java
@@ -0,0 +1,13 @@
+public class BankNonStrucure {
+    public static void main(String[] args) {
+        //data
+        int account_number = 12345; // số tk
+        int account_balance = 0; // số dư
+
+        //operation: thao tác lên dữ liệu
+        //deposit
+        // số tiền gửi được nhập từ bàn phím
+        
+        
+    }
+}
