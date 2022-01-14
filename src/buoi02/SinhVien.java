9843829f06a36c1046fbcb5e7ab74c7ea02eed59
Your Name <trangiahuytg1@gmail.com>
2022-01-14 09:08:32 +0700

    Đẩy lần 3

---
+++ src/buoi02/SinhVien.java
@@ -0,0 +1,19 @@
+package buoi02;
+
+import javax.print.event.PrintJobListener;
+
+public class SinhVien {
+    String ten;
+    String mssv;
+    String lop;
+    String khoa;
+    String nganh;
+    
+
+    void Hoc(){
+        System.out.println("Học:...");
+    }
+    void thamgiahoatdong(){
+        System.out.println("tham gia hoat động:.");
+    }
+}
---
+++ src/buoi02/SinhVienTestDrive.java
@@ -0,0 +1,14 @@
+package buoi02;
+
+public class SinhVienTestDrive {
+    public static void main(String[] args) {
+        SinhVien sinhvien;
+        sinhvien = new SinhVien();
+
+        
+        sinhvien.show();
+
+        sinhvien.Hoc();
+
+    }
+}
